f2eb4dfa23b68dade351a2ffdc9c24ab6b745ea1
Pedro Guedes <pedicoq@gmail.com>
2019-12-19 21:02:10 -0300

    mantém comentado as variáveis utilizadas dentro do aws lambda

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const apiKeys = require("./api-keys.json");
 const twit = new Twit({
   timeout_ms: 60 * 1000,
   ...apiKeys
+  // consumer_key: process.env.consumer_key,
+  // consumer_secret: process.env.consumer_secret,
+  // access_token: process.env.access_token,
+  // access_token_secret: process.env.access_token_secret,
 });
 
 const universidades = [
@@ -58,3 +62,5 @@ exports.handler = async () => {
     }
   }
 };
+
+// exports.handler();
